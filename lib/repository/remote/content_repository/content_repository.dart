687ec829bf804fe1aa676d1c8918262927a59d84
Yacine <y.driss@esi-sba.dz>
2023-02-10 18:38:21 +0100

    Add Content repo (api calls to get app announces and portfolios)

--- lib/repository/remote/content_repository/content_repository.dart
+++ lib/repository/remote/content_repository/content_repository.dart
@@ -1,17 +1,29 @@
+import 'dart:convert';
+
+import 'package:architect_hub/model/announce_model.dart';
+import 'package:architect_hub/model/portfolio_model.dart';
 import 'package:architect_hub/repository/remote/content_repository/base_content_repository.dart';
 import 'package:flutter/services.dart';
 
 class ContentRepository extends BaseContentRepository {
   @override
-  Future<void> getAnnounces() async {
+  Future<List<AnnounceModel>> getAnnounces() async {
     final String response =
         await rootBundle.loadString('assets/dummy_data/announces.json');
-    print(response);
+    final data = jsonDecode(response);
+    List<AnnounceModel> list = AnnounceModel.listFromJson(data);
+    return list;
   }
 
   @override
-  Future<void> getPortfolios() {
-    // TODO: implement getPortfolios
-    throw UnimplementedError();
+  Future<List<PortifolioModel>> getPortfolios() async {
+    return await Future.delayed(const Duration(milliseconds: 1500))
+        .then((value) async {
+      final String response =
+          await rootBundle.loadString('assets/dummy_data/portfolios.json');
+      final data = jsonDecode(response);
+      List<PortifolioModel> list = PortifolioModel.listFromJson(data);
+      return list;
+    });
   }
 }
